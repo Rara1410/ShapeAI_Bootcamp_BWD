7ccb5970d5a73258386a69782154bdcdb736d5dc
Rahul Raj <67092147+Rara1410@users.noreply.github.com>
2021-06-27 21:35:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Info from "./Info";
+import Footer from "./Footer"
+function App()
+{
+  return(
+    <div>
+      <Header />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info()
+{
+  return(
+    <div className="note">
+     <h1>Javascript And React.js</h1>
+     <p>Its An Bootcamp On basic React.js For 7 Days</p>
+    </div>
+  );
+}
+export default Info;
